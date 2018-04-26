6402dc2efc4f1e59823f1d74f58fb77db54c6ad7
ChuangshiJiang <30435246+ChuangshiJiang@users.noreply.github.com>
2018-04-26 09:48:22 +0800

    TestOnReading

---
+++ serializeTest/js/EventUtil.js
@@ -0,0 +1,46 @@
+var EventUtil = {
+    //添加事件处理程序
+    addHandler:function(element,type,handler){
+        if(element.addEventListener){
+            element.addEventListener(type,handler,false);
+        }else if(element.attachEvent){
+            element.attachEvent("on"+type,handler);
+        }else{
+            element["on"+type] = handler;
+        }
+    },
+    //移除事件处理程序
+    removeHandler:function(element,type,handler){
+        if(element.removeEventListener){
+            element.removeEventListener(type,handler,false);
+        }else if(element.detachEvent){
+            element.detachEvent("on"+type,handler);
+        }else{
+            element["on"+type] = null;
+        }
+    },
+    //获取事件对象
+    getEvent:function(event){
+        return event || window.event;
+    },
+    //获取目标元素
+    getTarget:function(event){
+        return event.target || event.srcElement;
+    },
+    //阻止默认事件
+    preventDefault:function(event){
+        if(event.preventDefault){
+            event.preventDefault();
+        }else{
+            event.returnValue = false;
+        }
+    },
+    //阻止事件冒泡
+    stopPropagation:function(event){
+        if(event.stopPropagation){
+            event.stopPropagation();
+        }else{
+            event.cancelBubble = true;
+        }
+    }
+};
